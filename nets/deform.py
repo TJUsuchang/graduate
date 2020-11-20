6d92e1820d7fe4879344b0c404c740f62fddc70f
suchang <576232417@qq.com>
2020-11-20 17:17:57 +0800

    [exp] [modify] [add cbam to deformbottleneck] [add cd&sa in resnet.py --> cannot import name 'DeformBottleneck' from 'nets.deform']

--- nets/deform.py
+++ nets/deform.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
+import torch
 
 from nets.deform_conv import DeformConv, ModulatedDeformConv
-
 
 def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
     """3x3 convolution with padding"""
@@ -13,6 +13,40 @@ def conv1x1(in_planes, out_planes, stride=1):
     """1x1 convolution"""
     return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
 
+class ChannelAttention(nn.Module):
+    def __init__(self, in_planes, ratio=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+
+        self.fc1   = nn.Conv2d(in_planes, in_planes // 16, 1, bias=False)
+        self.relu1 = nn.ReLU()
+        self.fc2   = nn.Conv2d(in_planes // 16, in_planes, 1, bias=False)
+
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = self.fc2(self.relu1(self.fc1(self.avg_pool(x))))
+        max_out = self.fc2(self.relu1(self.fc1(self.max_pool(x))))
+        out = avg_out + max_out
+        return self.sigmoid(out)
+
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+
+        assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
+        padding = 3 if kernel_size == 7 else 1
+
+        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv1(x)
+        return self.sigmoid(x)
 
 class DeformConv2d(nn.Module):
     """A single (modulated) deformable conv layer"""
@@ -108,6 +142,10 @@ class DeformBottleneck(nn.Module):
         self.conv3 = conv1x1(width, planes * self.expansion)
         self.bn3 = norm_layer(planes * self.expansion)
         self.relu = nn.ReLU(inplace=True)
+
+        self.ca = ChannelAttention(planes * 4)
+        self.sa = SpatialAttention()
+
         self.downsample = downsample
         self.stride = stride
 
@@ -124,6 +162,9 @@ class DeformBottleneck(nn.Module):
 
         out = self.conv3(out)
         out = self.bn3(out)
+
+        out = self.sa(out) * out
+        out = self.ca(out) * out
 
         if self.downsample is not None:
             identity = self.downsample(x)
