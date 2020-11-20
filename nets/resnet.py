62e3f5d8505b387f68508f88be98b70d1ac13b28
suchang <576232417@qq.com>
2020-11-20 11:48:41 +0800

    [exp] [resnet_cbam]

--- nets/resnet.py
+++ nets/resnet.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch
 
 from nets.deform import DeformBottleneck
 
@@ -13,6 +14,40 @@ def conv1x1(in_planes, out_planes, stride=1):
     """1x1 convolution"""
     return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
 
+class ChannelAttention(nn.Module):
+    def __init__(self, in_planes, ratio=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+
+        self.fc1   = nn.Conv2d(in_planes, in_planes // 16, 1, bias=False)
+        self.relu1 = nn.ReLU()
+        self.fc2   = nn.Conv2d(in_planes // 16, in_planes, 1, bias=False)
+
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = self.fc2(self.relu1(self.fc1(self.avg_pool(x))))
+        max_out = self.fc2(self.relu1(self.fc1(self.max_pool(x))))
+        out = avg_out + max_out
+        return self.sigmoid(out)
+
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+
+        assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
+        padding = 3 if kernel_size == 7 else 1
+
+        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv1(x)
+        return self.sigmoid(x)
 
 class BasicBlock(nn.Module):
     expansion = 1
@@ -33,6 +68,10 @@ class BasicBlock(nn.Module):
         self.relu = nn.ReLU(inplace=True)
         self.conv2 = conv3x3(planes, planes)
         self.bn2 = norm_layer(planes)
+
+        self.ca = ChannelAttention(planes)
+        self.sa = SpatialAttention()
+
         self.downsample = downsample
         self.stride = stride
 
@@ -45,6 +84,9 @@ class BasicBlock(nn.Module):
 
         out = self.conv2(out)
         out = self.bn2(out)
+
+        out = self.sa(out) * out
+        out = self.ca(out) * out
 
         if self.downsample is not None:
             identity = self.downsample(x)
@@ -73,6 +115,10 @@ class Bottleneck(nn.Module):
         self.conv3 = conv1x1(width, planes * self.expansion)
         self.bn3 = norm_layer(planes * self.expansion)
         self.relu = nn.ReLU(inplace=True)
+
+        self.ca = ChannelAttention(planes * 4)
+        self.sa = SpatialAttention()
+
         self.downsample = downsample
         self.stride = stride
 
@@ -89,6 +135,9 @@ class Bottleneck(nn.Module):
 
         out = self.conv3(out)
         out = self.bn3(out)
+
+        out = self.sa(out) * out
+        out = self.ca(out) * out
 
         if self.downsample is not None:
             identity = self.downsample(x)
