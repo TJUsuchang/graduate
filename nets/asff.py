3a8d38a73fc8ea2de9ff14cd9d34a558a2c23c9e
tjulyz <15620955923@163.com>
2020-12-28 14:28:59 +0800

    [exp] [asff same as csa for dowm/up]

--- nets/asff.py
+++ nets/asff.py
@@ -41,9 +41,7 @@ class ASFF(nn.Module):
             self.expand = add_conv(self.inter_dim, 32, 3, 1)
         elif level == 2:
             self.compress_level_0 = add_conv(16, self.inter_dim, 1, 1)
-            # self.stride_level_0 = add_conv(self.inter_dim, self.inter_dim // 2, 3, 1)
             self.compress_level_1 = add_conv(32, self.inter_dim, 1, 1)
-            # self.stride_level_1 = add_conv(self.inter_dim, self.inter_dim // 2, 3, 1)
             self.expand = add_conv(self.inter_dim, 64, 3, 1)
 
         compress_c = 8 if rfb else 16  #when adding rfb, we use half number of channels to save memory
@@ -76,22 +74,17 @@ class ASFF(nn.Module):
             level_1_resized = self.stride_level_1(x_level_1)
             level_2_compressed = self.stride_level_2(x_level_2)
             level_2_resized = F.interpolate(level_2_compressed, size=(x_level_0.size(2), x_level_0.size(3)), mode='bilinear')
-            # level_2_downsampled_inter =F.max_pool2d(x_level_2, 3, stride=2, padding=1)
-            # level_2_resized = self.stride_level_2(level_2_downsampled_inter)
 
         elif self.level == 1:
             level_0_compressed = self.compress_level_0(x_level_0)
             level_0_resized = F.interpolate(level_0_compressed, size=(x_level_1.size(2), x_level_1.size(3)), mode='bilinear')
-            # level_0_resized = F.interpolate(level_0_compressed, scale_factor=2, mode='nearest')
             level_1_resized = x_level_1
             level_2_resized = self.stride_level_2(x_level_2)
         elif self.level == 2:
             level_0_compressed = self.compress_level_0(x_level_0)
             level_0_resized = F.interpolate(level_0_compressed, size=(x_level_2.size(2), x_level_2.size(3)), mode='bilinear')
-            # level_0_resized = F.interpolate(level_0_compressed, scale_factor=4, mode='nearest')
             level_1_compressed = self.compress_level_1(x_level_1)
             level_1_resized = F.interpolate(level_1_compressed, size=(x_level_2.size(2), x_level_2.size(3)), mode='bilinear')
-            # level_1_resized = F.interpolate(level_1_compressed, scale_factor=2, mode='nearest')
             level_2_resized = x_level_2
 
         cat0 = torch.cat((torch.max(level_0_resized, 1)[0].unsqueeze(1), torch.mean(level_0_resized, 1).unsqueeze(1)), dim=1)
