a25c6b82f50ee32fb2ee3c8b8b00c6efb240a76e
tjulyz <15620955923@163.com>
2021-01-18 22:39:45 +0800

    [exp] [atten-isa-csa]

--- nets/deform.py
+++ nets/deform.py
@@ -13,6 +13,45 @@ def conv1x1(in_planes, out_planes, stride=1):
     """1x1 convolution"""
     return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
 
+class globalpoolatten3(nn.Module):
+    def __init__(self):
+        super(globalpoolatten3, self).__init__()
+        self.conv1 = nn.Conv2d(2, 1, kernel_size=3, padding=1, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv1(x)
+        return self.sigmoid(x)
+
+class globalpoolatten5(nn.Module):
+    def __init__(self):
+        super(globalpoolatten5, self).__init__()
+        self.conv1 = nn.Conv2d(2, 1, kernel_size=5, padding=2, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv1(x)
+        return self.sigmoid(x)
+
+class globalpoolatten7(nn.Module):
+    def __init__(self):
+        super(globalpoolatten7, self).__init__()
+        self.conv1 = nn.Conv2d(2, 1, kernel_size=7, padding=3, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv1(x)
+        return self.sigmoid(x)
+
 class DeformConv2d(nn.Module):
     """A single (modulated) deformable conv layer"""
 
