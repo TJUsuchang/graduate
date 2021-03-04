2eaed9bf1d92ca8ed713961926f2a4cad5644430
tjulyz <15620955923@163.com>
2021-03-04 16:26:51 +0800

    exp fpn+finalcsa

--- nets/aggregation.py
+++ nets/aggregation.py
@@ -372,26 +372,6 @@ class AdaptiveAggregationModule(nn.Module):
                                                 nn.BatchNorm2d(max_disp // (2 ** i))))
                     self.fuse_layers[-1].append(nn.Sequential(*layers))
 
-        for i in range(self.num_scales):
-            if i == 0:
-                self.fuse_layers2.append(nn.Sequential(nn.Conv2d(64, 64, kernel_size=1, bias=False),
-                                         nn.BatchNorm2d(64),
-                                         nn.LeakyReLU(0.2, inplace=True)))
-            elif i == 1:
-                self.fuse_layers2.append(nn.Sequential(nn.Conv2d(32, 32, kernel_size=1, bias=False),
-                                         nn.BatchNorm2d(32),
-                                         nn.LeakyReLU(0.2, inplace=True)))
-                self.convb.append(nn.Sequential(nn.Conv2d(32, 64, kernel_size=1, bias=False),
-                                  nn.BatchNorm2d(64),
-                                  nn.LeakyReLU(0.2, inplace=True)))
-            elif i == 2:
-                self.fuse_layers2.append(nn.Sequential(nn.Conv2d(16, 16, kernel_size=1, bias=False),
-                                         nn.BatchNorm2d(16),
-                                         nn.LeakyReLU(0.2, inplace=True)))
-                self.conva.append(nn.Sequential(nn.Conv2d(16, 32, kernel_size=1, bias=False),
-                                  nn.BatchNorm2d(32),
-                                  nn.LeakyReLU(0.2, inplace=True)))
-
         self.relu = nn.LeakyReLU(0.2, inplace=True)
 
     def forward(self, x):
@@ -420,28 +400,10 @@ class AdaptiveAggregationModule(nn.Module):
                                                  mode='bilinear', align_corners=False)
                     x_fused[i] = x_fused[i] + exchange
 
-        for i in range(2, -1, -1):
-            if i == 2:
-                x_atten.append(self.fuse_layers2[i](x_fused[i]))
-                x_fused2.append(x_atten[-1])
-            elif i == 1:
-                exchange = F.interpolate(x_fused2[-1], scale_factor=2,
-                                     mode='bilinear', align_corners=False)
-                exchange = self.conva[-1](exchange)
-                x_atten.append(self.fuse_layers2[i](x[i]))
-                x_fused2.append(x_atten[-1] + exchange)
-            elif i == 0:
-                exchange = F.interpolate(x_fused2[-1], scale_factor=2,
-                                     mode='bilinear', align_corners=False)
-                exchange = self.convb[-1](exchange)
-                x_atten.append(self.fuse_layers2[i](x[i]))
-                x_fused2.append(x_atten[-1] + exchange)
-        x_fused2 = x_fused2[::-1]
+        for i in range(len(x_fused)):
+            x_fused[i] = self.relu(x_fused[i])
 
-        for i in range(len(x_fused2)):
-            x_fused2[i] = self.relu(x_fused2[i])
-
-        return x_fused2
+        return x_fused
 
 
 # Stacked AAModules
@@ -481,6 +443,29 @@ class AdaptiveAggregation(nn.Module):
 
         self.fusions = nn.Sequential(*fusions)
 
+        self.fuse_layers2 = nn.ModuleList()
+        self.conva = nn.ModuleList()
+        self.convb = nn.ModuleList()
+        for i in range(self.num_scales):
+            if i == 0:
+                self.fuse_layers2.append(nn.Sequential(nn.Conv2d(64, 64, kernel_size=1, bias=False),
+                                         nn.BatchNorm2d(64),
+                                         nn.LeakyReLU(0.2, inplace=True)))
+            elif i == 1:
+                self.fuse_layers2.append(nn.Sequential(nn.Conv2d(32, 32, kernel_size=1, bias=False),
+                                         nn.BatchNorm2d(32),
+                                         nn.LeakyReLU(0.2, inplace=True)))
+                self.convb.append(nn.Sequential(nn.Conv2d(32, 64, kernel_size=1, bias=False),
+                                  nn.BatchNorm2d(64),
+                                  nn.LeakyReLU(0.2, inplace=True)))
+            elif i == 2:
+                self.fuse_layers2.append(nn.Sequential(nn.Conv2d(16, 16, kernel_size=1, bias=False),
+                                         nn.BatchNorm2d(16),
+                                         nn.LeakyReLU(0.2, inplace=True)))
+                self.conva.append(nn.Sequential(nn.Conv2d(16, 32, kernel_size=1, bias=False),
+                                  nn.BatchNorm2d(32),
+                                  nn.LeakyReLU(0.2, inplace=True)))
+
         self.final_conv = nn.ModuleList()
         for i in range(self.num_scales):
             in_channels = max_disp // (2 ** i)
@@ -498,8 +483,28 @@ class AdaptiveAggregation(nn.Module):
             cost_volume = fusion(cost_volume)
 
         # Make sure the final output is in the first position
+        x_atten = []
+        pre_out = []
+        for i in range(2, -1, -1):
+            if i == 2:
+                x_atten.append(self.fuse_layers2[i](cost_volume[i]))
+                pre_out.append(x_atten[-1])
+            elif i == 1:
+                exchange = F.interpolate(pre_out[-1], scale_factor=2,
+                                     mode='bilinear', align_corners=False)
+                exchange = self.conva[-1](exchange)
+                x_atten.append(self.fuse_layers2[i](cost_volume[i]))
+                pre_out.append(x_atten[-1] + exchange)
+            elif i == 0:
+                exchange = F.interpolate(pre_out[-1], scale_factor=2,
+                                     mode='bilinear', align_corners=False)
+                exchange = self.convb[-1](exchange)
+                x_atten.append(self.fuse_layers2[i](cost_volume[i]))
+                pre_out.append(x_atten[-1] + exchange)
+        pre_out = pre_out[::-1]
+
         out = []  # 1/3, 1/6, 1/12
         for i in range(len(self.final_conv)):
-            out = out + [self.final_conv[i](cost_volume[i])]
+            out = out + [self.final_conv[i](pre_out[i])]
 
         return out
